4f9ec3e0e68e42c2fe9e576c9fc2298a641b2a3f
sohibxon <muhammadiyevsohibnazar@gmail.com>
2023-05-31 16:21:55 +0500

    add js file structure

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let firstname = "Sohibnazar"
+let surname = "Muhammadiyev"
+let age = 19
+let read = "TATU"
+let hobby = "phone"
+console.log("Mening ismim " + firstname  + "Mening familyam " + surname + "Mening yoshim " + age + "Men " + read + "da o`qiyman" + "Men " + hobby + "larga qiziqaman");
+
